749f0b3e53e8e3bf36fffbce28c3347f5b081070
Roland Ibiwoye <contact@rolandayo.com>
2024-10-30 13:07:29 +0100

    react

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -28,7 +28,7 @@ export default function Footer() {
           </div>
         </div>
         <div className="border-t border-gray-700 mt-8 pt-8 text-center">
-          <p className="text-gray-400">© 2024 Your Company. All rights reserved.</p>
+          <p className="text-gray-400">© 2024 Your rolandaayo. All rights reserved.</p>
         </div>
       </div>
     </footer>
